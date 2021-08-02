dcf3671c9b60c5e747568d2f95e9b9c7e1d65002
SalmanS <salmanshalahuddin133@gmail.com>
2021-08-03 02:38:56 +0700

    finished cart and transaction

--- src/components/cart/cart.js
+++ src/components/cart/cart.js
@@ -1,159 +1,168 @@
-import './cart.css'
-import { useState, useEffect } from 'react'
-import { withRouter } from 'react-router'
-import axios from 'axios'
+import "./cart.css";
+import { useState, useEffect } from "react";
+import { withRouter } from "react-router";
+import axios from "axios";
 
 const Cart = (props) => {
-  const { token, history } = props
-  const [cart, setCart] = useState([])
-  const [cartProduct, setCartProduct] = useState([])
-  const formatter = new Intl.NumberFormat('id-ID', {
-    style: 'currency',
-    currency: 'IDR'
+  const { token, history } = props;
+  const [cart, setCart] = useState([]);
+  const [cartProduct, setCartProduct] = useState([]);
+  const formatter = new Intl.NumberFormat("id-ID", {
+    style: "currency",
+    currency: "IDR",
   });
 
   useEffect(() => {
-      axios.get('http://localhost:8080/cart', {
+    axios
+      .get("http://localhost:8080/cart", {
         headers: {
-          'auth-token': token
-        }
-      }).then((res) =>
-        setCart(res.data)
-      ).catch(e => console.log(e.request));          
-  }, [])
+          "auth-token": token,
+        },
+      })
+      .then((res) => setCart(res.data))
+      .catch((e) => console.log(e.request));
+  }, []);
 
   useEffect(() => {
-      for (let i = 0; i < cart.cartProducts?.length; i++) {
-        getCartProductDetail(cart.cartProducts[i].productId)
-      }        
-  }, [cart])
+    for (let i = 0; i < cart.cartProducts?.length; i++) {
+      getCartProductDetail(cart.cartProducts[i].productId);
+    }
+  }, [cart]);
 
   const updateCartProduct = () => {
-    axios.get('http://localhost:8080/cart', {
+    axios
+      .get("http://localhost:8080/cart", {
         headers: {
-          'auth-token': token
-        }
-      }).then((res) =>
-        setCart(res.data)
-      ).catch(e => console.log(e.request));
-  }
+          "auth-token": token,
+        },
+      })
+      .then((res) => setCart(res.data))
+      .catch((e) => console.log(e.request));
+  };
 
   const getCartProductDetail = (id) => {
-    axios.get(`http://localhost:8080/product/${id}`)
-      .then((res) => setCartProduct(array => [...array, res.data]))
-      .catch((e) => console.log(e.message))
-  }
+    axios
+      .get(`http://localhost:8080/product/${id}`)
+      .then((res) => setCartProduct((array) => [...array, res.data]))
+      .catch((e) => console.log(e.message));
+  };
 
   const handleRemoveCartProduct = async (prodId) => {
     if (window.confirm("Remove product from cart?")) {
       axios.delete(`http://localhost:8080/cart/${prodId}`, {
         headers: {
-          'auth-token': token
-        }
-      })
-      setCartProduct([])
-    }  
-    await updateCartProduct() 
-  }
+          "auth-token": token,
+        },
+      });
+      setCartProduct([]);
+    }
+    await updateCartProduct();
+  };
+
+  const updateCartAmount = async () => {
+    try {
+      await axios.put(`http://localhost:8080/cart/update`, cart, {
+        headers: {
+          "auth-token": token,
+        },
+      });
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
 
   const handleIncrement = (index) => {
-    let tempCart = cart
+    let tempCart = cart;
 
     if (cartProduct[index].stock === tempCart.cartProducts[index].amount) {
-      document.getElementsByClassName("plus-button").disabled = true
+      document.getElementsByClassName("plus-button").disabled = true;
+    } else {
+      tempCart.cartProducts[index].amount += 1;
+      setCart({ ...tempCart });
+      updateCartAmount()
     }
-    else {
-      tempCart.cartProducts[index].amount += 1
-      setCart({...tempCart})
-    }
-  }
+  };
 
   const handleDecrement = (index) => {
-    let tempCart = cart
+    let tempCart = cart;
 
     if (tempCart.cartProducts[index].amount === 1) {
-      handleRemoveCartProduct(tempCart.cartProducts[index].productId)   
+      handleRemoveCartProduct(tempCart.cartProducts[index].productId);
+    } else {
+      tempCart.cartProducts[index].amount -= 1;
+      setCart({ ...tempCart });
+      updateCartAmount()
     }
-    else {      
-      tempCart.cartProducts[index].amount -= 1
-      setCart({...tempCart})
-    }
-  }
-
-  const toogleSelectAll = () => {
-    if (document.getElementById("select-all-checkbox").checked === true) {
-      document.getElementById("product-checkbox").checked = true;
-    }
-    else {
-      document.getElementById("product-checkbox").checked = false;
-    }
-  }  
+  };
 
   const getTotalPrice = () => {
-    let sum = 0
+    let sum = 0;
 
     if (cartProduct.length > 0) {
-      cartProduct.map((elem) => {
-        return sum += elem.price
-      })
+      cart.cartProducts.map((elem, index) => {
+        return (sum += (cartProduct[index]?.price * elem.amount));
+      });
     }
 
-    return(
-     <h2>Total : {formatter.format(sum)}</h2>
-    )
-  }
+    return <h2>Total : {formatter.format(sum)}</h2>;
+  };
 
   const handleCheckout = () => {
     if (cartProduct.length > 0) {
-      history.push('/checkout')
+      history.push("/checkout");
     } else {
-      document.getElementsByClassName("checkout-btn").disabled = true
+      document.getElementsByClassName("checkout-btn").disabled = true;
     }
-  }
-  
+  };
+
   const showProduct = () => {
-    console.log(cartProduct)
-    if (cartProduct.length > 0) {      
+    if (cartProduct.length > 0) {
       return cart.cartProducts.map((elem, index) => {
         return (
-          <div className="product-cart-wrapper">              
+          <div className="product-cart-wrapper">
             <div key={cartProduct[index]?._id} className="product-cart-item">
-              <input type="checkbox" id="product-checkbox" />
-              <img src={process.env.PUBLIC_URL + '/images/product/gambar_belum_ada.jpg'} alt="" />                
+              <img
+                src={
+                  process.env.PUBLIC_URL +
+                  "/images/product/gambar_belum_ada.jpg"
+                }
+                alt=""
+              />
               <div className="product-info-cart">
                 <h3>{cartProduct[index]?.product_name}</h3>
                 <p>{formatter.format(cartProduct[index]?.price)}</p>
-                <button className="minus-button" onClick={() => handleDecrement(index)}>-</button>
+                <button
+                  className="minus-button"
+                  onClick={() => handleDecrement(index)}
+                >
+                  -
+                </button>
                 <span> {elem.amount} </span>
-                <button className="plus-button" onClick={() => handleIncrement(index)}>+</button>
+                <button
+                  className="plus-button"
+                  onClick={() => handleIncrement(index)}
+                >
+                  +
+                </button>
               </div>
-            </div>                
+            </div>
           </div>
-        )
-      }) 
+        );
+      });
+    } else {
+      return <p>The Cart is Empty...</p>;
     }
-    else {
-      return (
-        <p>The Cart is Empty...</p>        
-      )      
-    }
-  }
+  };
 
   return (
     <div className="cart-container">
       <h1>Cart</h1>
-      <div className="product-cart-list">
-        {showProduct()}       
-      </div>      
-      <div className="select-all-cart">
-        <input type="checkbox" id="select-all-checkbox" onClick={toogleSelectAll} />
-        <h3>Select All</h3>
-      </div>
-      <div className="total-price">
-        {getTotalPrice()}
-      </div>
-      <button className="checkout-btn" onClick={() => handleCheckout()}><span>CHECKOUT</span></button>
+      <div className="product-cart-list">{showProduct()}</div>
+      <div className="total-price">{getTotalPrice()}</div>
+      <button className="checkout-btn" onClick={() => handleCheckout()}>
+        <span>CHECKOUT</span>
+      </button>
     </div>
-  )
-}
+  );
+};
 export default withRouter(Cart);
--- src/components/checkout/checkout.js
+++ src/components/checkout/checkout.js
@@ -1,16 +1,272 @@
-import './checkout.css'
-import { useState, useEffect } from 'react'
-import axios from 'axios'
+import "./checkout.css";
+import { useState, useEffect } from "react";
+import { useFormik } from "formik";
+import axios from "axios";
+import { nanoid } from "nanoid";
 
 const Checkout = (props) => {
-  const { token } = props
-  const [checkout, setCheckout] = useState()
+  const { token } = props;
+  const [cart, setCart] = useState([]);
+  const [cartProduct, setCartProduct] = useState([]);
+  const [user, setUser] = useState([])
+  let totalPrice = 0    
+  const formatter = new Intl.NumberFormat("id-ID", {
+    style: "currency",
+    currency: "IDR",
+  });
 
-  return(
-    <div>
-      <h1>TES</h1>
+  useEffect(() => {
+    axios.get("http://localhost:8080/cart", {
+        headers: {
+          "auth-token": token,
+        },
+      })
+      .then((res) => setCart(res.data))
+      .catch((e) => console.log(e.request));
+    
+    axios.get("http://localhost:8080/user", {
+        headers: {
+          "auth-token": token,
+        },
+      })
+      .then((res) => setUser(res.data))
+      .catch((e) => console.log(e.request));
+  }, []);
+
+  useEffect(() => {
+    for (let i = 0; i < cart.cartProducts?.length; i++) {
+      getCartProductDetail(cart.cartProducts[i].productId);
+    }
+  }, [cart]);
+
+  const clearCartList = async () => {
+    return await axios.delete(`http://localhost:8080/cart`, {
+      headers: {
+        "auth-token": token,
+      },
+    })
+  }
+
+  const getCartProductDetail = (id) => {
+    axios.get(`http://localhost:8080/product/${id}`)
+      .then((res) => setCartProduct((array) => [...array, res.data]))
+      .catch((e) => console.log(e.message));
+  };
+
+  const handlePostTransaction = async (data) => {
+    return await axios.post(`http://localhost:8080/checkout/all`, data, {
+      headers: {
+        "auth-token": token,
+      },
+    })
+  }
+  
+  const orderInfo = useFormik({
+    initialValues: {
+      province: user?.address?.province ||"",
+      city: user?.address?.city||"",
+      street: user?.address?.street||"",
+      zip_code: user?.address?.zip_code||"",
+      paymentMethod: "",
+    },
+    enableReinitialize: true,    
+    onSubmit: async (values) => {   
+      // const shipmentCost = Math.floor(Math.random() * 40000) + 20000
+      const finalPrice = totalPrice+22000      
+      const finalObject = {
+        address: {
+          province: values.province,
+          city: values.city,
+          street: values.street,
+          zip_code: values.zip_code
+        },
+        payment_method: values.paymentMethod,
+        checkoutProduct: cart?.cartProducts,
+        shipment_cost: 22000,
+        shipment_receipt: nanoid(),
+        total_price: finalPrice
+      }
+      try {
+        await handlePostTransaction(finalObject)
+        await clearCartList()
+        alert("ORDER SUCCESS, ENJOY!")
+      } catch {
+        alert("Transaction failed")
+      }
+    },
+  });
+
+  const showOrderInfoInput = () => {
+    return (
+      <div className="order-info">
+        {/* RECIPIENT ADDRESS */}
+        <form onSubmit={orderInfo.handleSubmit}>
+          <h2>Recipient Address</h2>
+          <div className="form-alamat">
+            <div>
+              <label htmlFor="province">Province</label>
+              <input
+                id="province"
+                name="province"
+                type="text"
+                onChange={orderInfo.handleChange}
+                value={orderInfo.values.province}
+              />
+              <br />
+            </div>
+            <div>
+              <label htmlFor="city">City</label>
+              <input
+                id="city"
+                name="city"
+                type="text"
+                onChange={orderInfo.handleChange}
+                value={orderInfo.values.city}
+              />
+              <br />
+            </div>
+            <div>
+              <label htmlFor="street">Street</label>
+              <input
+                id="street"
+                name="street"
+                type="text"
+                onChange={orderInfo.handleChange}
+                value={orderInfo.values.street}
+              />
+              <br />
+            </div>
+            <div>
+              <label htmlFor="zip_code">ZIP Code</label>
+              <input
+                id="zip_code"
+                name="zip_code"
+                type="text"
+                onChange={orderInfo.handleChange}
+                value={orderInfo.values.zip_code}
+              />
+              <br />
+            </div>
+          </div>
+
+          {/* PAYMENT METHOD*/}
+          <h2>Payment Method</h2>
+          <div className="payment-method-checkout">
+            <div>
+              <input
+                required
+                id="credit-card"
+                name="paymentMethod"
+                type="radio"
+                onChange={orderInfo.handleChange}
+                value="credit-card"
+              />
+              <label htmlFor="credit-card">Credit Card</label>
+              <br />
+            </div>
+            <div>
+              <input
+                id="debit-card"
+                name="paymentMethod"
+                type="radio"
+                onChange={orderInfo.handleChange}
+                value="debit-card"
+              />
+              <label htmlFor="debit-card">Debit Card</label>
+              <br />
+            </div>
+            <div>
+              <input
+                id="transfer"
+                name="paymentMethod"
+                type="radio"
+                onChange={orderInfo.handleChange}
+                value="transfer"
+              />
+              <label htmlFor="transfer">Transfer</label>
+              <br />
+            </div>
+          </div>
+
+          <div className="make-order">
+            {checkProductStock()}
+            <h2>Total : {formatter.format(totalPrice+22000)}</h2>
+            <button className="make-order-btn" type="submit">
+              Make Order
+            </button>
+          </div>
+        </form>
+      </div>
+    );
+  };
+
+  const showProduct = () => {
+    if (cartProduct.length > 0) {
+      return cart.cartProducts.map((elem, index) => {
+        return (
+          <div className="product-checkout-wrapper">
+            <div
+              key={cartProduct[index]?._id}
+              className="product-checkout-item"
+            >
+              <img
+                src={
+                  process.env.PUBLIC_URL +
+                  "/images/product/gambar_belum_ada.jpg"
+                }
+                alt=""
+              />
+              <div className="product-info-checkout">
+                <h3>{cartProduct[index]?.product_name}</h3>
+                <span> Quantity : {elem.amount} </span>
+                <p>
+                  Total Price : {formatter.format(cartProduct[index]?.price*elem.amount)}
+                </p>
+              </div>
+            </div>
+          </div>
+        );
+      });
+    } else {
+      return <p>The Cart is Empty...</p>;
+    }
+  };
+
+  const showOrderDetail = () => {
+    return (
+      <div className="order-detail">
+        <h2>Order Detail</h2>
+        <p>Subtotal for product : {formatter.format(totalPrice)}</p>
+        <p>Subtotal for delivery : {formatter.format(22000)}</p>
+      </div>
+    );
+  };
+
+  const getTotalPrice = () => {
+    let sum = 0;
+
+    if (cartProduct.length > 0) {
+      cart.cartProducts.map((elem, index) => {
+        return (sum += (cartProduct[index]?.price * elem.amount));
+      });
+    }
+    
+    totalPrice = sum
+  };
+
+  const checkProductStock = () => {
+    
+  }
+
+  return (
+    <div className="checkout-wrapper">
+      <h1>Checkout</h1>
+      {getTotalPrice()}
+      {showProduct()}
+      {showOrderDetail()}
+      {showOrderInfoInput()}
     </div>
-  )
-}
+  );
+};
 
-export default Checkout
+export default Checkout;
